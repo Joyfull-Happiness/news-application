cd0ecb1ff837c0ac20d5f36a21caf2dc6ed8cd0f
Joyfull-Happiness <lajoie.bradley2@gmail.com>
2025-08-01 14:22:21 -0700

    updated card

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 import { useState } from 'react';
-import Card from './components/Card';
+import NewsCard from './components/NewsCard.jsx';
+import Form from './components/Form.jsx';
 import './App.css';
 
 function App() {
-  const [city, setCity] = useState('');
-  const [weatherData, setWeatherData] = useState(null);
-  const apiKey = '00e156e27c2c6797ab845a53fea21367';
+  // const [city, setCity] = useState('');
+  const [newsData, setNewsData] = useState(null);
+  const apiKey = '54c87cf8b7362dfe1c3a42e7e009b805';
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+      `https://gnews.io/api/v4/top-headlines?category=general&lang=en&country=us&max=10&apikey=${apiKey}`
     )
       .then((res) => res.json())
-      .then((data) => setWeatherData(data))
+      .then((data) => setNewsData(data))
       .catch((err) => console.error('API error:', err));
   };
 
@@ -40,7 +41,7 @@ function App() {
         </button>
       </form>
 
-      {weatherData && <Card weather={weatherData} />}
+      {newsData && <NewsCard newsProp={newsData} />}
     </div>
   );
 }
---
+++ src/components/NewsCard.jsx
@@ -0,0 +1,30 @@
+
+function NewsCard({ newsProp }) {
+    //weather is the entire data obj
+    console.log('weather', weather);
+  
+    //explore what weather.weather[0] looks like in the console
+    const { main, description, icon } = weather.weather[0];
+  
+    //explore what weather.main looks like in the console
+    const { temp_max, temp_min } = weather.main;
+  
+    
+    // render the card in the return below. Use the same classNames if you want the styling to apply
+    return (
+      <div className="card">
+        <h2>{weather.name}</h2>
+        <img src={iconUrl} alt={description} />
+        <h3>{main}</h3>
+        <p className="description">{description}</p>
+        {/* Notice the difference between the temp without rounding vs with rounding */}
+        <p>High: {temp_max}°F</p>
+        <p>Low: {Math.round(temp_min)}°F</p>
+      </div>
+    );
+  }
+  
+  
+
+
+export default NewsCard; 
