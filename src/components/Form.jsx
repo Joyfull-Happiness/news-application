67a4d196f2efa6bc3fddfaed85c2acfc218e76ce
Joyfull-Happiness <lajoie.bradley2@gmail.com>
2025-08-02 01:43:51 -0700

    updated form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,16 @@
 
+<form onSubmit={handleSubmit} className="form">
+<input
+  type="text"
+  placeholder="Enter a city"
+  value={city}
+  onChange={handleChange}
+  className="input"
+/>
+<button type="submit" className="button">
+  Get Weather
+</button>
+</form>
 
 
 
