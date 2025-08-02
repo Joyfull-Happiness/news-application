b0841a044cdf83151d36f3cfcfbce4644be4c530
Joyfull-Happiness <lajoie.bradley2@gmail.com>
2025-08-02 08:56:22 -0700

    updated form with values

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,14 +1,21 @@
 
 <form onSubmit={handleSubmit} className="form">
-<input
-  type="text"
-  placeholder="Enter a city"
-  value={city}
-  onChange={handleChange}
-  className="input"
-/>
+<form class="mood-form">
+      <label for="mood">Choose a Category:</label>
+      <select id="mood" name="mood">
+        <option value="">--Choose One--</option>
+        <option value="general">General</option>
+        <option value="world">World</option>
+        <option value="nation">Nation</option>
+        <option value="business">Buisness</option>
+        <option value="technology">Technology</option>
+        <option value="entertainment">Entertainment</option>
+        <option value="sports">Sports</option>
+        <option value="science">Science</option>
+        <option value="health">Health</option>
+      </select>
 <button type="submit" className="button">
-  Get Weather
+  Get News
 </button>
 </form>
 
